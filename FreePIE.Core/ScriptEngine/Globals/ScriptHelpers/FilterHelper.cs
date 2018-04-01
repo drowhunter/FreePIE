33f069b519a865b7bbd00dfd9e4d4712bdede82e
TrevorJones <jones1876@gmail.com>
2018-04-01 02:53:20 -0400

    scurve and jcurve filters

--- FreePIE.Core/ScriptEngine/Globals/ScriptHelpers/FilterHelper.cs
+++ FreePIE.Core/ScriptEngine/Globals/ScriptHelpers/FilterHelper.cs
@@ -117,6 +117,178 @@ namespace FreePIE.Core.ScriptEngine.Globals.ScriptHelpers
         {
             return Math.Max(Math.Min(((x - xMin)/(xMax - xMin))*(yMax - yMin) + yMin, yMax), yMin);
         }
+        #region Target Funcions
+        private struct sAxis
+        {
+            
+            public char dir;
+                                                     
+            //char locked;
+            //char relative;
+            //int trim;
+            int val;
+
+            public CurveMode curveMode;
+            /// <summary>
+            /// S curve parameters
+            /// </summary>
+            public int lower,center,upper,curve;
+            /// <summary>
+            /// J curve parameter, zoom for Scurve
+            /// </summary>
+            public double ab;
+
+        }
+
+        public enum CurveMode
+        {
+            None, SCurve, JCurve, Custom
+        }
+
+        const int AMAX = 32767, AMAXF = 32768;
+        double fcurve(double x, double lower, double center, double upper, double trim, int curve)
+        {
+            double m, M, cM, cm;
+            m = lower + lower - 1;
+            M = 1 - upper - upper;
+            cM = center;
+            cm = -cM;
+            if (x < m)
+                x = -1;
+            else if (x < cm)
+                if (curve == 0)
+                    x = (x - cm) / (cm - m);
+                else
+                    x = (1 - Math.Exp((cm - x) * curve)) / (Math.Exp((cm - m) * curve) - 1);
+            else if (x < cM)
+                x = 0;
+            else if (x < M)
+                if (Math.Abs(curve) < 0.01)
+                    x = (x - cM) / (M - cM);
+                else
+                    x = (Math.Exp((x - cM) * curve) - 1) / (Math.Exp((M - cM) * curve) - 1);
+            else
+                x = 1;
+            x = x + trim;
+
+            if (x < -1)
+                x = -1;
+            else if (x > 1)
+                x = 1;
+            return x;
+        }
+        double P2Curve(double x, double a, double b, double c)
+        {
+            return a * x * x + b * x + c;
+        }
+
+        /// <summary>
+        /// linear interpolate
+        /// </summary>
+        double LI(double x, double y, double X, double Y, double v)
+        {
+            return ((Y - y) * v + X * y - x * Y) / (X - x);
+        } // linear interpolate
+        
+        double GetCustomCurveValue(int p, double v)
+        {
+            return v;
+            /*p = p + 1 & 0xffff;
+            int n = keyalloc[p];    // list end
+            int i = p + 1;
+            if (i >= n)
+                return v;
+            while (i < n)
+                if (keyalloc[i] < v)
+                    i = i + 2;
+                else if (i == p + 1)
+                    return LI(0, 0, keyalloc[i], keyalloc[i + 1], v);
+                else
+                    return LI(keyalloc[i - 2], keyalloc[i - 1], keyalloc[i], keyalloc[i + 1], v);
+            return LI(keyalloc[i - 2], keyalloc[i - 1], 100, 100, v);*/
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="val">input value</param>
+        /// <param name="inval">percents</param>
+        /// <param name="outval">percents</param>
+        /// <returns></returns>
+        double SetJCurve(double val, double inval, double outval) // in, out = percents
+        {
+            var axdata = new sAxis();
+            axdata.curveMode = CurveMode.JCurve;
+            axdata.ab = 50 * (inval - outval) / (inval * (inval - 100));
+            return AxisVal(val, axdata);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="val">input value</param>
+        /// <param name="lower">percent</param>
+        /// <param name="center">percent</param>
+        /// <param name="upper">percent</param>
+        /// <param name="curve">-32..32</param>
+        /// <param name="zoom">percent</param>
+        /// <returns></returns>
+        public double SetSCurve(double val,int lower = 0, int center = 0, int upper = 0, int curve = 0, double zoom = 0) 
+        {
+            var axdata = new sAxis
+            {
+                curveMode = CurveMode.SCurve,
+                lower = lower,
+                center = center,
+                upper = upper,
+                curve = curve,
+                ab = zoom
+            };
+            return AxisVal(val, axdata);           
+        }
+
+        /*
+        public struct Axis
+        {
+            public int coupling;
+            public double cos, sin;
+
+            public Axis(int yAxis, double v1, double v2) : this()
+            {
+            }
+        }
+        int RotateDXAxis(int XAxis, int YAxis, double angle) // clockwise angle, in degrees
+        {
+            angle = angle * Math.PI / 180;
+            var xAxis = new Axis(YAxis, Math.Cos(angle), -Math.Sin(angle));
+
+            Axis[XAxis].coupling = YAxis;
+            Axis[XAxis].cos = Math.Cos(angle);
+            Axis[XAxis].sin = -Math.Sin(angle);
+            Axis[YAxis].coupling = XAxis;
+            Axis[YAxis].cos = Math.Cos(angle);
+            Axis[YAxis].sin = Math.Sin(angle);
+        }*/
+        private double AxisVal(double v, sAxis d)
+        {
+            switch (d.curveMode)
+            {
+                case CurveMode.SCurve:
+                    v = AMAX * Math.Pow(1.41, d.ab) * fcurve(v / AMAXF, d.lower * 0.01, d.center * 0.01, d.upper * 0.01, 0, d.curve);
+                    break;
+                case CurveMode.JCurve:
+                    v = AMAX * P2Curve(v / AMAXF, -d.ab, 1, d.ab);
+                    break;
+                case CurveMode.Custom://TODO
+                    v = AMAX * GetCustomCurveValue((int)d.curveMode, v * 50 / AMAXF + 50) / 50 - AMAX; 
+                    break;
+               
+            }
+
+            return v * (1 + d.dir);
+        }
+
+        #endregion
 
         [NeedIndexer]
         public bool stopWatch(bool state, int milliseconds, string indexer)
